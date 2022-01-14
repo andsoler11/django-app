35547bb9917aa2adc5975788ea1e7cb8539233aa
Andres Soler <andres.soler@talent.com>
2022-01-14 14:26:16 -0500

    starting to add messages options

--- users/models.py
+++ users/models.py
@@ -40,3 +40,30 @@ class Skill(models.Model):
 
 
 
+class Message(models.Model):
+    # el que envia el mensaje esta asociado a un perfil, por eso se usa el foreign key
+    # colocamos null en on delete ya que si una persona borra su perfil queremos aun ver sus mensajes
+    sender = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True)
+    recipient = models.ForeignKey(Profile, on_delete=models.SET_NULL, null=True, blank=True, related_name='messages')
+    name = models.CharField(max_length=200, null=True, blank=True)
+    email = models.EmailField(max_length=200, null=True, blank=True)
+    subject = models.CharField(max_length=200, null=True, blank=True)
+    body = models.TextField()
+    is_read = models.BooleanField(default=False, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.subject
+
+    class Meta:
+        ordering = ['is_read', '-created']
+
+    
+
+
+
+
+
+
+
--- users/views.py
+++ users/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 from django.db.models import Q
 
 from projects.views import projects
-from .models import Profile, Skill
+from .models import Profile, Skill, Message
 from .forms import CustomUserCreationForm, ProfileForm, SkillForm
 from .utils import searchProfiles, paginateProfiles
 # Create your views here.
@@ -180,6 +180,7 @@ def updateSkill(request, pk):
     return render(request, 'users/skill_form.html', context)
 
 
+@login_required(login_url='login')
 def deleteSkill(request, pk):
     profile = request.user.profile
     skill = profile.skill_set.get(id=pk)
@@ -191,3 +192,12 @@ def deleteSkill(request, pk):
 
     context = {'object': skill}
     return render(request, 'delete_template.html', context)
+
+
+@login_required(login_url='login')
+def inbox(request):
+    profile = request.user.profile
+    message_request = profile.messages.all()
+    unread_count = message_request.filter(is_read=False).count()
+    context = {'message_request': message_request, 'unread_count': unread_count}
+    return render(request, 'users/inbox.html', context)
