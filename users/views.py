897c7daccd39a397e1967b97d8be6f99d1f929f2
Andres Soler <andres.soler@talent.com>
2022-01-14 15:08:44 -0500

    finished messages and ocnnect to postresql db

--- users/views.py
+++ users/views.py
@@ -7,7 +7,7 @@ from django.db.models import Q
 
 from projects.views import projects
 from .models import Profile, Skill, Message
-from .forms import CustomUserCreationForm, ProfileForm, SkillForm
+from .forms import CustomUserCreationForm, ProfileForm, SkillForm, MessageForm
 from .utils import searchProfiles, paginateProfiles
 # Create your views here.
 
@@ -201,3 +201,42 @@ def inbox(request):
     unread_count = message_request.filter(is_read=False).count()
     context = {'message_request': message_request, 'unread_count': unread_count}
     return render(request, 'users/inbox.html', context)
+
+
+@login_required(login_url='login')
+def viewMessage(request, pk):
+    profile = request.user.profile
+    message = profile.messages.get(id=pk)
+    if message.is_read == False:
+        message.is_read = True
+        message.save()
+    context = {'message': message}
+    return render(request, 'users/message.html', context)
+
+
+def createMessage(request, pk):
+    recipient = Profile.objects.get(id=pk)
+    form = MessageForm()
+
+    try:
+        sender = request.user.profile
+    except:
+        sender = None
+
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.sender = sender
+            message.recipient = recipient
+
+            if sender:
+                message.name = sender.name
+                message.email = sender.email
+            message.save()
+
+            messages.success(request, 'Your message was successfully sent!')
+            return redirect('user-profile', pk=recipient.id)
+
+    context = {'recipient': recipient, 'form': form}
+    return render(request, 'users/message_form.html', context)
